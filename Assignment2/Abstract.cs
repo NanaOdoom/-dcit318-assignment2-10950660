b2a89e7551d3bd840e9d846161bda0d441541d1d
NanaOdoom <yaawsley@gmail.com>
2024-06-17 13:55:15 +0000

    Abstract Classes and Methods

---
+++ Assignment2/Abstract.cs
@@ -0,0 +1,29 @@
+using System;
+namespace Assignment2
+{
+    abstract class Shape{
+        public abstract double GetArea();
+        class Circle : Shape{
+            public double Radius { get; set; } 
+
+            public override double GetArea(){
+                return Math.PI * Math.Pow(Radius,2);
+            }
+        }
+        class Rectangle : Shape{
+            public double Lenght { get; set; }
+            public double Width { get; set; }
+
+            public override double GetArea(){
+                return Width * Lenght;
+            }
+        }
+        static void Main(string[] args){
+            Circle circle = new Circle(){Radius = 5};
+            circle.GetArea();
+
+            Rectangle rectangle = new Rectangle(){ Lenght = 8, Width = 8};
+            rectangle.GetArea();
+        }
+    }
+}
